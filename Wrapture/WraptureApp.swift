a4704ed456ddc7349b96a51ffd8fb2551491cd00
Rob Anderson <knurl@me.com>
2026-09-11 16:51:18 +0100

    Initial Commit

---
+++ Wrapture/WraptureApp.swift
@@ -0,0 +1,17 @@
+//
+//  WraptureApp.swift
+//  Wrapture
+//
+//  Created by Rob Anderson on 11/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct WraptureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
