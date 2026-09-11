99e64662aed95a89e29b346fabc4b033c55c9e37
Rob Anderson <knurl@me.com>
2026-09-11 23:27:22 +0100

    Fix tests

--- WraptureExt/SourceEditorExtension.swift
+++ WraptureExt/SourceEditorExtension.swift
@@ -1,12 +1,4 @@
-//
-//  SourceEditorExtension.swift
-//  WraptureExt
-//
-//  Created by Rob Anderson on 11/09/2026.
-//
-
 import Foundation
-import XcodeKit
 
-class SourceEditorExtension: NSObject, XCSourceEditorExtension {
+class SourceEditorExtension: NSObject {
 }
